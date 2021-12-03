df3f68aa8ab32c52af1e4d823e9f012917becb72
Siddharth Bidikar <siddharthbidikar@Siddharths-MacBook-Pro.local>
2021-12-03 18:08:05 -0500

    added equipment page

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = ({onClick}) => {
+
+
+    return (
+        <div>
+          <button onClick={onClick}>Submit</button>  
+        </div>
+    )
+}
+
+export default Button
--- src/components/Equipments.js
+++ src/components/Equipments.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import Button from './Button'
+import { useState } from 'react'
 
 const Equipments = () => {
+    const [id, setId] = useState(Math.floor(Math.random() * 10000) + 1)
+    const [name, setName] = useState('')
+    const [description, setDescription] = useState('')
+    const [deskId, setDeskId] = useState('')
+    const onClick = () => {
+        console.log(this.name)
+        console.log(this.description)
+        console.log(this.deskId)
+    }
+
+    const onSubmit = (e) => {
+        if(!deskId){
+            alert("Enter Desk Id")
+        }
+    }
+
     return (
         <div>
-            "Equipment form"
+            <form onSubmit={onSubmit}>
+                <div>
+                    <label>Equipment Name</label>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
+                </div>
+                <div>
+                    <label>Equipment Description</label>
+                    <input type="text" value={description} onChange={(e) => setDescription(e.target.value)}></input>
+                </div>
+                <div>
+                    <label>Desk Id</label>
+                    <input type="text" value={deskId} onChange={(e) => setDeskId(e.target.value)}></input>
+                </div>
+                <div>
+                    <input type="submit" value="Order Equipment"></input>
+                </div>
+            
+            </form>
         </div>
     )
 }
