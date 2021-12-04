7570e555c32bf2239be5e59778b08b60cbf6e2ae
Siddharth Bidikar <siddharthbidikar@Siddharths-MacBook-Pro.local>
2021-12-03 19:19:54 -0500

    updated equipment form

--- src/components/Equipments.js
+++ src/components/Equipments.js
@@ -3,7 +3,7 @@ import Button from './Button'
 import { useState } from 'react'
 
 const Equipments = () => {
-    const [id, setId] = useState(Math.floor(Math.random() * 10000) + 1)
+    const [itemId, setItemId] = useState(Math.floor(Math.random() * 10000) + 1)
     const [name, setName] = useState('')
     const [description, setDescription] = useState('')
     const [deskId, setDeskId] = useState('')
@@ -14,29 +14,52 @@ const Equipments = () => {
     }
 
     const onSubmit = (e) => {
+        e.preventDefault()
         if(!deskId){
             alert("Enter Desk Id")
         }
+        const data = {itemId,name,description,deskId}
+        fetch('http://localhost:8080/openofficeapi/rent',{
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(data)
+
+        }).then(() => {
+            console.log("Equipment request submitted")
+        })
+        console.log(data)
+    }
+
+    const addEquipment = async() => {
+        const res = await fetch('http://localhost:8080/openofficeapi/rent')
+        const data = await res.json()
+        console.log(data)
     }
 
     return (
-        <div>
+        <div className="create">
+            <h2>Order Equipment Form</h2>
             <form onSubmit={onSubmit}>
                 <div>
-                    <label>Equipment Name</label>
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
+                    <label>Equipment Name: </label>
+                    <select value={name} onChange={(e) => setName(e.target.value)}>
+                        <option value="monitor">Monitor</option>
+                        <option value="mouse">Mouse</option>
+                        <option value="keyboard">Keyboard</option>
+                    </select>
                 </div>
                 <div>
-                    <label>Equipment Description</label>
-                    <input type="text" value={description} onChange={(e) => setDescription(e.target.value)}></input>
+                    <label>Equipment Description: </label>
+                    <textarea type="text" value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
                 </div>
                 <div>
-                    <label>Desk Id</label>
+                    <label>Desk Id: </label>
                     <input type="text" value={deskId} onChange={(e) => setDeskId(e.target.value)}></input>
                 </div>
                 <div>
                     <input type="submit" value="Order Equipment"></input>
                 </div>
+                <p>{name}</p>
             
             </form>
         </div>
